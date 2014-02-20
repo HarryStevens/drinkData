7c4263621fcc664ba92ffb23cfdd7dd25b0e8aa6
HarryStevens <harryjosephstevens@gmail.com>
2014-02-20 13:20:08 -0500

    added general outline to js file

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/**
+ * @author Hazard
+ */
+
+/*
+ * This javascript file will accomplish three distinct tasks.
+ * 
+ * 1.) It will use the document ready function to call another function
+ * 2.) The other funciton will create a new div w/ text in it, which will be added to index.html. 
+ * 		It will also get the JSON data from DRINKS_DATA.json. 
+ * 3.) It will have an event handler function for loading the JSON, which will have a console.log 
+ * 		to put some data on the page.
+ */
